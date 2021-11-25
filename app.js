63b7a809e4e0466df8887c9569ddafdb771b55cf
bhardwajrizul <bhardwajrizul9816@gamil.com>
2021-11-25 20:27:41 +0530

    Create sign-in sign-up Page

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const bodyParser = require("body-parser");
 
 
 const app = express();
+
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
 
 app.get("/", (req, res) => {
-    res.send("This is response");
+    res.render("signin", {});
+});
+app.get("/signup", (req, res)=>{
+    res.render("signup", {});
 });
 
 
